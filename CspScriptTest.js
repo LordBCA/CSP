041065d667ac93a2a382ce24e93b29f6cacb4ae1
Diego Bocanegra <diegoa_boca@hotmail.com>
2023-05-17 09:42:05 -0500

    Update CspScriptTest.js

--- CspScriptTest.js
+++ CspScriptTest.js
@@ -2,4 +2,18 @@ $(document).ready(function () {
   $("#btn").click(function (){
     alert("Alert displayed using a external script.");
   });
+  $("#btnApi").click(function (){
+    var options = {};
+        options.url = "https://weatherapi-com.p.rapidapi.com/";
+        options.type = "GET";
+        options.dataType = "json";
+        options.success = function (response) {
+            alert("Api response: "+ response);            
+        };
+
+        options.error = function () {
+            alert("Api error: "+ response);              
+        };
+        $.ajax(options);    
+  });
 });
